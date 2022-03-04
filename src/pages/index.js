3fe3428627ac9c7c42d37faf9ceec5c1d46337ea
AGW <agiraldow@gmail.com>
2022-03-04 15:20:26 -0500

    Add site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,27 @@
 import React from "react"
 import styles from "../styles/global.css"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
-  return <div className={styles}>
-      <div class="logo">Saint Warren</div>
-      <div class="description-and-links">
-        <p>
+  return <><Helmet>
+    <title>Saint Warren</title>
+  </Helmet>
+  <div className={styles}>
+    <div class="logo">Saint Warren</div>
+    <div class="description-and-links">
+      <p>
         Saint Warren is both a perfume line and the alias of Brooklyn-based DJ/visual artist, Warren Mark Way.
-        </p>
-        <p>
-          <a href="https://instagram.com/saint_warren" target="new">
+      </p>
+      <p>
+        <a href="https://instagram.com/saint_warren" target="new">
           @saint_warren
-          </a>
-        </p>
-      </div>
-      <div class="perfume-and-warren">
-        <img src="./img/IMG_0874.jpeg"></img>
-        <img src="./img/IMG_0948.jpeg" class="warren"></img>
-      </div>
+        </a>
+      </p>
     </div>
+    <div class="perfume-and-warren">
+      <img src="./img/IMG_0874.jpeg"></img>
+      <img src="./img/IMG_0948.jpeg" class="warren"></img>
+    </div>
+  </div></>
 }
 
