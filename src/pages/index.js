06f8608fe194d53fddc05d95c0304fe5b4277379
AGW <agiraldow@gmail.com>
2022-03-04 14:46:52 -0500

    Update favicon, colors, and make responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,21 @@ import styles from "../styles/global.css"
 
 export default function Home() {
   return <div className={styles}>
-    <div class="logo">Saint Warren</div>
-    
+      <div class="logo">Saint Warren</div>
+      <div class="description-and-links">
+        <p>
+        Saint Warren is both a perfume line and the alias of Brooklyn-based DJ/visual artist, Warren Mark Way.
+        </p>
+        <p>
+          <a href="https://instagram.com/saint_warren" target="new">
+          @saint_warren
+          </a>
+        </p>
+      </div>
+      <div class="perfume-and-warren">
+        <img src="./img/IMG_0874.jpeg"></img>
+        <img src="./img/IMG_0948.jpeg" class="warren"></img>
+      </div>
     </div>
 }
 
